200fa1c0d3d6aad2a2f1ca0693f397bd2df8302f
Amruta Vikas Adhav <166296307+amrutaadhav@users.noreply.github.com>
2026-09-13 11:25:44 +0530

    Update sumOfPrimeNoUptoN.java

--- sumOfPrimeNoUptoN.java
+++ sumOfPrimeNoUptoN.java
@@ -1,10 +1,20 @@
 import java.util.*;
-public class sumOfPrimeNoUptoN {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-
-
-
-    sc.close();
-  }
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int sum = 0;
+       for(int i=2;i<=n;i++){
+           int count = 0;
+           for(int j=1;j<=i;j++){
+              if(i % j==0){
+                  count ++;
+               }
+           }
+         if(count==2){
+            sum = sum + i;
+           }
+       }
+       System.out.print(sum); 
+    }
 }
